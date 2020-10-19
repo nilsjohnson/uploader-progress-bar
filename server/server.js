328f8e2088a17887f63715c43daa075addfb1a3e
Nils <nilsjohnson328@gmail.com>
2020-10-19 12:23:24 -0400

    commented out line for serving the build directory

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ require('./uploadAPI');
 // if you want to serve the build from this server, you can do so by 
 // uncommenting this line. For dev puroposes (taking advatage of hot reloading)
 // we don't want to serve the build .
-app.use(express.static(path.join(__dirname, '../build'), { index: 'index.html' }));
+// app.use(express.static(path.join(__dirname, '../build'), { index: 'index.html' }));
 
 // listen for API requests
 http.listen(PORT_NUM, () => {
